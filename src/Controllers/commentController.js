9267feedec19f1bd71723078d2ce20baadde5740
AL-Amin008 <mamin.193008@bscse.uiu.ac.bd>
2023-09-11 18:37:05 +0600

    third

---
+++ src/Controllers/commentController.js
@@ -0,0 +1,35 @@
+exports.create = async (req, res) => {
+    try {
+        
+        res.status(201).json({ status: "success", data: "Comment created successfully" });
+    } catch (error) {
+        res.status(500).json({ status: "error", message: "Internal server error" });
+    }
+};
+
+exports.read = async (req, res) => {
+    try {
+        
+        res.status(200).json({ status: "success", data: "Comment data goes here" });
+    } catch (error) {
+        res.status(500).json({ status: "error", message: "Internal server error" });
+    }
+};
+
+exports.delete = async (req, res) => {
+    try {
+       
+        res.status(204).json();
+    } catch (error) {
+        res.status(500).json({ status: "error", message: "Internal server error" });
+    }
+};
+
+exports.update = async (req, res) => {
+    try {
+        
+        res.status(200).json({ status: "success", data: "Comment updated successfully" });
+    } catch (error) {
+        res.status(500).json({ status: "error", message: "Internal server error" });
+    }
+};
